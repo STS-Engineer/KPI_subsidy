cdd16230c206dfef67970b8fdc614e6f512f2fef
STS-Engineer <administration.sts@avocarbon.com>
2025-11-03 10:01:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1142,8 +1142,8 @@ scheduler = BackgroundScheduler(timezone=pytz.timezone('Africa/Tunis'))
 scheduler.add_job(
     scheduled_email_task,
     'cron',
-    hour=9,
-    minute=30,
+    hour=10,
+    minute=10,
     timezone=pytz.timezone('Africa/Tunis'),
     id='kpi_email_scheduler',
     name='KPI Automated Email Scheduler'
